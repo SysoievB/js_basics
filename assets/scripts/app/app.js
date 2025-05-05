04b9549e3b86fd645e8730865870eb1c3311649e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-05-05 16:06:50 +0200

    Added app

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -1,10 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    const calcDescription = `${currentResult} + ${userInput.value}`;
-    currentResult = currentResult + parseInt(userInput.value);
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteResult(operator, resultBeforeCalculation, calcNumb) {
+    const calcDescription = `${resultBeforeCalculation} ${operator} ${calcNumb}`;
     outputResult(currentResult, calcDescription);
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteResult('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteResult('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteResult('*', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteResult('/', initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
