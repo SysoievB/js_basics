b38ddc8490ba60a1219162fc5f375c5be67afccd
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-05-13 17:11:03 +0200

    Added array`s creation

---
+++ assets/scripts/array_methods.js
@@ -0,0 +1,13 @@
+//array creation
+const arr = [1, 2, 3];
+console.log(arr)//[1, 2, 3]
+const arr2 = new Array('hey', 'hello');
+console.log(arr2);//[ 'hey', 'hello' ]
+
+const arr3 = Array(5);//empty array with size 5
+const arr4 = Array.from("hi!");//transforms string to array
+console.log(arr4);//[ 'h', 'i', '!' ]
+
+const arr5 = [1, "hello", {name: 'vasia'}];//can contain diff types of elements
+console.log(arr5);//[ 1, 'hello', { name: 'vasia' } ]
+
