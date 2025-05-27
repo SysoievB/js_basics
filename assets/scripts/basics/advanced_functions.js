7494ae9ba926f638d687e150e40337b2eb34d698
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-05-27 17:09:03 +0200

    Added advanced functions

---
+++ assets/scripts/basics/advanced_functions.js
@@ -0,0 +1,47 @@
+//pure function
+//Given the same input, always returns the same output.
+function add(a, b) {
+    return a + b;
+}
+
+//impure function
+//Depends on or modifies external state.
+let counter = 0;
+function increment() {
+    counter++;
+}
+
+//Factory Function - returns a new object each time when it's called
+function createMe(age, name) {
+    return {
+        name,
+        age
+    }
+}
+console.log(createMe(25, 'Vasia'));
+
+//Closure - A function that "remembers" the variables from its outer scope, even after the outer function has finished
+function outer() {
+    let count = 0;
+    return function inner() {//inner() remembers count even after outer() is done. That’s closure.
+        count++;
+        console.log(count);
+    };
+}
+
+const counter1 = outer();
+counter1(); // 1
+counter1(); // 2
+
+//Pattern IIFE - Immediately Invoked Function Expression(in older scripts)
+(function() {
+    var age = 30;
+    console.log(age); // 30
+})()
+//console.log(age); // Error: "age is not defined"
+//the same as now
+{
+    const age = 30;
+    console.log(age); // 30
+}
+//console.log(age); // Error: "age is not defined"
